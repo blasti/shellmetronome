e5337dfaa5bb45bd6edbc8ed8e426d9d9b86fece
blastbeat <blasti@users.noreply.github.com>
2022-03-10 00:10:58 +0100

    Small fixes

--- metronome.sh
+++ metronome.sh
@@ -3,12 +3,14 @@
 # metronome.sh by blastbeat
 # works on my Debian...
 
-if [[ ! "$1" =~ ^[[:digit:]]+$ ]]; then
-    echo "Usage: metronome.sh beats-per-minute" >&2
+if [[ ! "$1" =~ ^[[:digit:]]+$ ]]
+then
+    printf "%s\\n" "Usage: metronome.sh beats-per-minute" >&2
     exit 1
 fi
 
-while true; do
+while true
+do
     aplay -q beat.wav &
     sleep "$(bc <<< "scale=2; 60/$1")"
 done
